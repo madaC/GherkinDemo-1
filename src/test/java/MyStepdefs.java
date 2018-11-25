15713a6d65d1e7404ccea440f1745eeccc6f52d5
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-11-25 09:26:58 +0200

    first

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,78 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class MyStepdefs {
+
+    @Given("^I'm in tablets search results$")
+    public void iMInTabletsSearchResults() throws Throwable {
+
+    }
+
+    @When("^filter by price lower than (\\d+)$")
+    public void filterByPriceLowerThan(int arg0) throws Throwable {
+       
+    }
+
+    @Then("^Only tablets with lower price will be presented$")
+    public void onlyTabletsWithLowerPriceWillBePresented() throws Throwable {
+
+    }
+
+    @Then("^Only tablets with lower price will be presented Only tablets with lower price will be presented Only tablets with lower price will be presented$")
+    public void onlyTabletsWithLowerPriceWillBePresentedOnlyTabletsWithLowerPriceWillBePresentedOnlyTabletsWithLowerPriceWillBePresented() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Given("^I am in Octane$")
+    public void iAmInOctane() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("^Clicking the New Feature$")
+    public void clickingTheNewFeature() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("^Feature dialog is open$")
+    public void featureDialogIsOpen() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Given("^I am in ALM Octane$")
+    public void iAmInALMOctane() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+  
+//
+//    @Given("^a calculateCommercialMargin request$")
+//    public void aCalculateCommercialMarginRequest() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//    }
+//
+//    @When("^a valid request is send$")
+//    public void aValidRequestIsSend() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//    }
+//
+//    @Then("^a response with no error is expected$")
+//    public void aResponseWithNoErrorIsExpected() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//    }
+//
+//    @And("^the loanProductType,loanAmount,loanAmountCurrency,repaymentType,repaymentMaturity,interestPeriodIndicator,interestPeriodicity,capitalPeriodicity,purposeCode,purposeCodeType,interestRateCalculationBasisNumerator,interestRateCalculationBasisDenominator,drawDownStartDate,loanLifecycleStatus,yearlyRealProductMargin,yearlyRealIndividualMargin,yearlyRealTheoreticalGlobalMargin,yearlyRealApplicableGlobalMargin,selectedPriceFlag,customerExternalId,operationStatus,code,message are not null$")
+//    public void the_loanProductType_loanAmount_loanAmountCurrency_repaymentType_repaymentMaturity_interestPeriodIndicator_interestPeriodicity_capitalPeriodicity_purposeCode_purposeCodeType_interestRateCalculationBasisNumerator_interestRateCalculationBasisDenominator_drawDownStartDate_loanLifecycleStatus_yearlyRealProductMargin_yearlyRealIndividualMargin_yearlyRealTheoreticalGlobalMargin_yearlyRealApplicableGlobalMargin_selectedPriceFlag_customerExternalId_operationStatus_code_message_are_not_null() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//    }
+
+}
